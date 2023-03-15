6319c710a59fc88e77899f7a3713c045828f7c22
Konik Rahman <113472401+RahmanKonik@users.noreply.github.com>
2023-03-16 00:45:16 +0200

    added the sample swift code

---
+++ String interpolation example.swift
@@ -0,0 +1,8 @@
+
+var age = 26 // using the var and assign the value
+print("You are \(age) years old") // using the print function for print the content.
+
+
+age = 36 // just change the age value  and print it using  string interpolation, backlack'\'
+
+print("You are \(age) years old")
